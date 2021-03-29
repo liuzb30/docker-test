f481dd8cb84ee53b454f20a0a1e3230f8917f1ce
liu zhenbin <536543269@qq.com>
2021-03-29 12:32:18 +0800

    feat: test

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <p>hello docker</p>
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <p>hello docker2</p>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
